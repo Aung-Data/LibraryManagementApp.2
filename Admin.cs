faa5547031a49961663e9fab8d8b616c575a9131
LouieP12 <louiepoland@gmail.com>
2026-06-02 16:28:40 +1200

    Update Admin.cs

--- Admin.cs
+++ Admin.cs
@@ -3,14 +3,14 @@ namespace FinalProject
     internal class Admin : Login //Admin inherits from Login class 
     {
         // Constructor
-        public Admin(string username, string password) : base(username, password) //Calls the base class (Login) constructor and passes its properties.
+        public Admin(string username, string password) : base(username, password) //Calls the base class constructor and forwards its properties.
         { 
         }
 
         //Display Admin Menu 
-        public override string DisplayMenu() //override replaces version in Login
+        public override string DisplayMenu() //override replaces version Login
         {
-            Console.WriteLine("\nAdmin Menu:\n1. Add a Book\n2. Remove a Book\n3. View all the Books\n4. Search for a Book\n5. Manage Acccount/s\n6. View Library Info\n99. Sign Out");
+            Console.WriteLine("\nAdmin Menu:\n1. Add Book\n2. Remove Book\n3. View all the Books\n4. Search for a Book\n5. Manage Acccount/s\n6. View Library Info\n99. Sign Out");
             return "";
         }
         
