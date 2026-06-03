f43ab14a4a3e3441b58310cd34df48097166ea26
Louie Poland <Louie.Poland01@gmail.com>
2026-06-04 11:35:44 +1200

    including the inheritance and polymorphism features

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementApp._2
+{
+    public class Person
+    {
+        //Person class properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Person class constructor
+        public Person(string fn, string ln)
+        {
+            FirstName = fn;
+            LastName = ln;
+        }
+
+        //Virtual method for polymorphism
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+}
